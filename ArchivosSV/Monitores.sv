ec1d2107a206e7f6b7f9f1ee4200d9d862bdea7c
Sebastián Barrantes <sbarrantes0909@gmail.com>
2025-10-20 15:05:33 -0600

    Correcciones

--- ArchivosSV/Monitores.sv
+++ ArchivosSV/Monitores.sv
@@ -160,6 +160,7 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
 
 
       if ((vif.md_rx_data != last_data_rx) & vif.md_rx_ready) begin
+        $display("[MD_MON] Cambio de dato RX detectado");
         // Cierra el "dato activo" anterior
         MD_Rx_Sample #(ALGN_DATA_WIDTH) sample = new();
         sample.data_in = vif.md_rx_data;
@@ -179,6 +180,7 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
       end
 
       else if (vif.md_rx_ready) begin
+        $display("[MD_MON] Handshake RX detectado");
         MD_Rx_Sample #(ALGN_DATA_WIDTH) handshake_tr = new();
         handshake_tr.data_in = vif.md_rx_data;
         handshake_tr.offset = vif.md_rx_offset;
@@ -219,6 +221,7 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
       @(posedge vif.clk);
       // === 2) Detección de CAMBIO DE DATO (aunque no haya handshake) ===
       if ((vif.md_tx_data != last_data_tx) & vif.md_tx_valid) begin
+        $display("[MD_MON] Cambio de dato TX detectado");
         // Cierra el "dato activo" anterior
         MD_Tx_Sample #(ALGN_DATA_WIDTH) change_tr = new();
         change_tr.data_out = vif.md_tx_data;
@@ -233,6 +236,7 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
         last_size_tx = vif.md_tx_size;
       end
       else if (vif.md_tx_valid) begin
+        $display("[MD_MON] Handshake TX detectado");
         MD_Tx_Sample #(ALGN_DATA_WIDTH) handshake_tr = new();
         handshake_tr.data_out = vif.md_tx_data;
         handshake_tr.ctrl_offset = vif.md_tx_offset;
@@ -261,7 +265,8 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
     forever begin
       wait (data_out_buffer.size() > 0);
       tx_sample = data_out_buffer.pop_front();
-      while (rx_bytes_available() < tx_sample.ctrl_size) begin
+      while (rx_bytes_available() != 0) begin
+        $display("[MD_MON] Enviado paquete MD al scoreboard/checker: %0d bytes", tx_sample.ctrl_size);
         @(posedge vif.clk);
         tr = new();
         tr.data_out = tx_sample;
