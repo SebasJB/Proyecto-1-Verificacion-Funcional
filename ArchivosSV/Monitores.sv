7c6e96b6429fa1086e06278472f0326f3a3f8e4d
Sebastián Barrantes <sbarrantes0909@gmail.com>
2025-10-23 17:23:03 -0600

    Correciones monitores

--- ArchivosSV/Monitores.sv
+++ ArchivosSV/Monitores.sv
@@ -184,8 +184,8 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
       rx_sample = data_in_buffer.pop_front();
       if (data_out_buffer.size() == 0 ) @ev_tx_pushed;
       tx_sample = data_out_buffer.pop_front();
-      $display("[MON] Datos en el buffer de entrada: %0d", data_in_buffer.size());
-      $display("[MON] Datos en el buffer de salida: %0d", data_out_buffer.size());
+      $display("[MON] Dato de entrada obtenido: %0d", rx_sample.data_in);
+      $display("[MON] Dato de salida obtenido : %0d", tx_sammple.data_out);
       if (rx_sample.size > tx_sample.ctrl_size) begin
         tx_bytes_count = 0;
         if (rx_sample.err) begin
