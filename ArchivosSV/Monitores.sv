b8b5857366f4ad1f26eb27ebbf23b9aaa3063d44
Sebastián Barrantes <sbarrantes0909@gmail.com>
2025-10-23 20:46:15 -0600

    Correciones monitores

--- ArchivosSV/Monitores.sv
+++ ArchivosSV/Monitores.sv
@@ -184,7 +184,7 @@ class MD_Monitor #(int ALGN_DATA_WIDTH = 32);
       
       if (data_out_buffer.size() == 0 ) @ev_tx_pushed;
       tx_sample = data_out_buffer.pop_front();
-      if rx_sample = data_in_buffer.pop_front();
+      if (data_in_buffer.size() != 0) rx_sample = data_in_buffer.pop_front();
       foreach (data_in_buffer[i]) begin
               $display("  [RX%0d] data=%h off=%0d size=%0d", i, data_in_buffer[i].data_in, data_in_buffer[i].offset, data_in_buffer[i].size);
             end
